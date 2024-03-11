f5c34b7b64cde01d95634590d805469df151be08
xkfe <zkai98516@gmail.com>
2024-03-11 22:58:45 +0800

    feat: add dark/light mode transition

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,5 @@
-import BlogTheme from '@sugarat/theme'
+import { h } from 'vue'
+import BlogLayout from '../components/BlogLayout.vue'
 
 // 自定义样式重载
 import './style.scss'
@@ -6,4 +7,9 @@ import './style.scss'
 // 自定义主题色
 import './user-theme.css'
 
-export default BlogTheme
+export default {
+  // @ts-ignore
+  Layout: h(BlogLayout, undefined, {
+    'layout-bottom': () => ({}) ,
+  })
+}
